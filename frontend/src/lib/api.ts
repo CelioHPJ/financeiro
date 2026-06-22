ab45ad12d9c3aabc0c1a7ceef8fdd13b78838133
CelioHPJ <celiohenrique3434@gmail.com>
2026-06-22 16:05:24 -0300

    fix enviroment variables

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-export const api = axios.create({
-  baseURL: 'http://localhost:3333/api',
+// CORRETO - Isso lê a variável que configuramos na Vercel
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
 });
-
 // Adiciona o token em todas as requisições
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('@ControleGastos:token');
-  
+
   if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-  
+
   return config;
 });
